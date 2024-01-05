32790537dd45f4d8a8b174341645f63d4dee30ea
MysteryArc <wanghyuuuuu@163.com>
2024-01-05 23:56:06 +0800

    优化

--- 基数排序.cpp
+++ 基数排序.cpp
@@ -3,46 +3,56 @@
  * @Author     : WangHengyu [wanghyuuuuu@163.com]
  * @CreateTime : 2024/01/03 16:44:21
  * 基于计数排序实现基数排序
-*/
-#include<iostream>
-#include<vector>
-#include<cmath>
+ */
+#include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-void radixSort(vector<int> &nums){
+void radixSort(vector<int> &nums)
+{
     int maxDigit = 0;
-    for(int i = 0; i < nums.size(); i++){
+    // 计算所有数字中的最大位数
+    for (int i = 0; i < nums.size(); i++)
+    {
         int digit = floor(log10(abs(nums[i]))) + 1;
         maxDigit = max(maxDigit, digit);
     }
-    for(int i = 0; i < maxDigit; i++){
+    // 有几位数就进行几次基数排序
+    for (int i = 0; i < maxDigit; i++)
+    {
         vector<int> count(10, 0);
         // 计数
-        for(int j = 0; j < nums.size(); j++){
+        for (int j = 0; j < nums.size(); j++)
+        {
             int num = nums[j] / int(pow(10, i)) % 10;
             count[num]++;
         }
         // 根据计数数组计算位置数组(结束位置的后一位)
-        for(int j = 1; j < 10; j++){
+        for (int j = 1; j < 10; j++)
+        {
             count[j] = count[j] + count[j - 1];
         }
         // 创建新数组并插入数据,由于记录的是结束位置的后一位，需要反向插入数据并在插入时将位置减一
         vector<int> res(nums.size());
-        for(int j = nums.size() - 1; j >= 0; j--){
+        for (int j = nums.size() - 1; j >= 0; j--)
+        {
             int num = nums[j] / int(pow(10, i)) % 10;
             res[--count[num]] = nums[j];
         }
         // 输出一轮排序结果
-        cout<<"第"<<i + 1<<"轮排序结果: ";
-        for(int j = 0; j < res.size(); j++){
-            cout<<res[j]<<" ";
+        cout << "第" << i + 1 << "轮排序结果: ";
+        for (int j = 0; j < res.size(); j++)
+        {
+            cout << res[j] << " ";
         }
-        cout<<endl;
+        cout << endl;
         nums = res;
     }
 }
 
-int main() {
+int main()
+{
     vector<int> a = {329, 457, 657, 839, 436, 720, 355};
     radixSort(a);
     return 0;
--- 快速排序.cpp
+++ 快速排序.cpp
@@ -2,27 +2,31 @@
  * @FileName   : 快速排序.cpp
  * @Author     : WangHengyu [wanghyuuuuu@163.com]
  * @CreateTime : 2024/01/03 14:16:04
-*/
-#include<iostream>
-#include<vector>
+ */
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void swap(vector<int> &nums, int i, int j){
+void swap(vector<int> &nums, int i, int j)
+{
     int temp = nums[i];
     nums[i] = nums[j];
     nums[j] = temp;
 }
 
-void quickSort(vector<int> &nums, int begin, int end) {
-    if(begin < end) {
+void quickSort(vector<int> &nums, int begin, int end)
+{
+    if (begin < end)
+    {
         int base = nums[begin];
         int i = begin, j = end;
-        while(i < j) {
-            while(nums[j] >= base && i < j)
+        while (i < j)
+        {
+            while (nums[j] >= base && i < j)
             {
                 j--;
             }
-            while(nums[i] <= base && i < j)
+            while (nums[i] <= base && i < j)
             {
                 i++;
             }
@@ -34,12 +38,14 @@ void quickSort(vector<int> &nums, int begin, int end) {
     }
 }
 
-int main() {
+int main()
+{
     vector<int> nums = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
     quickSort(nums, 0, nums.size() - 1);
-    for(int i = 0; i < nums.size(); i++){
-        cout<< nums[i] << " ";
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
